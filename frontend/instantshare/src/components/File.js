c86ce2c9a06f16cf1f06e3b7b165805bcc84acf0
Jonathan Doorn <jdoorn@purdue.edu>
2022-06-27 10:07:23 +0200

    converted ES6 file to function

--- frontend/instantshare/src/components/File.js
+++ frontend/instantshare/src/components/File.js
@@ -2,29 +2,25 @@ import React from "react";
 import { Button, Card, CloseButton } from "react-bootstrap";
 import Api from "../Api";
 
-class File extends React.Component {
+function File(props) {
 
-    constructor(props) {
-        super(props);
-        this.deleteFile = this.deleteFile.bind(this)
+    const deleteFile = () => {
+        props.onDelete(props.obj.id)
     }
-    deleteFile() {
-        this.props.onDelete(this.props.obj.id)
-    }
-    render() {
-        return (
-            <Card>
+
+    return (
+        <Card>
                 <Card.Header>
-                    <CloseButton onClick={this.deleteFile}/>
+                    <CloseButton onClick={deleteFile}/>
                 </Card.Header>
                 <Card.Body>
-                    <Card.Title>{this.props.obj.name}</Card.Title>
-                    <Card.Text>{this.props.obj.id}</Card.Text>
-                    <Button variant="primary" href={`http://localhost:5000/board/${this.props.boardid}/files/${this.props.obj.id}`}>Download</Button>
+                    <Card.Title>{props.obj.name}</Card.Title>
+                    <Card.Text>{props.obj.id}</Card.Text>
+                    <Button variant="primary" href={`http://localhost:5000/board/${props.boardid}/files/${props.obj.id}`}>Download</Button>
                 </Card.Body>
             </Card>
-        );
-    }
+    );
+
 }
 
 export default File;
